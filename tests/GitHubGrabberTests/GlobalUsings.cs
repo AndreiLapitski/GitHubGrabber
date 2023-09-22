7a719861a2be54c898ab688d245925808adc807a
Andrei Lapitski2 <Andrei_Lapitski2@epam.com>
2023-09-22 15:04:17 +0300

    Add tests for Filter()

--- tests/GitHubGrabberTests/GlobalUsings.cs
+++ tests/GitHubGrabberTests/GlobalUsings.cs
@@ -1,6 +1,7 @@
 global using GitHubGrabber.Clients;
 global using GitHubGrabber.Constants;
 global using GitHubGrabber.Enums;
+global using GitHubGrabber.Extensions;
 global using GitHubGrabber.Helpers;
 global using NUnit.Framework;
 global using System.Text.RegularExpressions;
---
+++ tests/GitHubGrabberTests/MatchCollectionExtensionTests.cs
@@ -0,0 +1,28 @@
+﻿namespace GitHubGrabberTests
+{
+    public class MatchCollectionExtensionTests
+    {
+        [Test]
+        [TestCase("CORE-12, ATS-8: qwe test-1, release-18",
+            new[] { "test", "release" },
+            new[] { "CORE-12", "ATS-8" })]
+        [TestCase("CORE-12, ATS-8: qwe test-1, release-18",
+            new[] { "tes" },
+            new[]{ "CORE-12", "ATS-8", "test-1", "release-18" })]
+        public void Filter_Success(
+            string str,
+            string[] forbiddenStrings,
+            string[] expectedStrings)
+        {
+            // Arrange
+            var pattern = OutputWorksheetConstants.LinkFromTitleRegex;
+            var regex = new Regex(pattern);
+
+            // Act
+            var resultStrings = regex.Matches(str).Filter(forbiddenStrings);
+
+            // Assert
+            Assert.That(resultStrings, Is.EqualTo(expectedStrings));
+        }
+    }
+}
